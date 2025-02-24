251abdc187d3002af3f8176ec9e2c2bbc7486e13
hmnneedhelp <ilovememesdontkillme@gmail.com>
2025-02-24 18:38:41 +0900

    init2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import './index.css'
-import App from './App.tsx'
 import {init, miniApp} from "@telegram-apps/sdk-react"
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 const initialize = async ()=>{
   try{
@@ -18,8 +32,12 @@ const initialize = async ()=>{
 
 initialize()
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
