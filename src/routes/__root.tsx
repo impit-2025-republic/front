d4d9508bf7964db803ca6d24aeecf6db04f96626
hmnneedhelp <ilovememesdontkillme@gmail.com>
2025-02-25 16:52:50 +0900

    asd

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
-      <div className={`mx-auto flex flex-col px-4 items-center min-h-screen pb-24`}>
+      <div className={`mx-auto flex flex-col px-4 bg-[#191B1F] items-center min-h-screen pb-24`}>
         <div className="w-[308px] px-4">
 
           <Outlet />
