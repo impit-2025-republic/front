dccd9541fa5e1e61b82314bfe480021323099cba
hmnneedhelp <ilovememesdontkillme@gmail.com>
2025-02-25 16:24:45 +0900

    афыв

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const useLogin =()=>useMutation({
     mutationKey:["login"],
     mutationFn:(data:any)=>{
-        const resp = axios.post('https://api.b8st.ru/login',{
+        const resp = axios.post('https://api.b8st.ru/login',{},{
             headers:{
                 Authorization:`${"tma "+data}`
             }
